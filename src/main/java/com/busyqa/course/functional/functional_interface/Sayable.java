1167dd579b52f34a7551b49731d31839aab5b780
Carlos Zegarra <zegarra@uwindsor.ca>
2019-11-15 16:57:19 -0500

    remove functional programming

--- src/main/java/com/busyqa/course/functional/functional_interface/Sayable.java
+++
@@ -1,6 +0,0 @@
-package com.busyqa.course.functional.functional_interface;
-
-@FunctionalInterface
-public interface Sayable{  
-    void say(String msg);  
-}  
--- src/main/java/com/busyqa/course/functional/lambda_expression/Sayable.java
+++
@@ -1,6 +0,0 @@
-package com.busyqa.course.functional.lambda_expression;
-
-@FunctionalInterface
-public interface Sayable {
-	public String say();
-}
